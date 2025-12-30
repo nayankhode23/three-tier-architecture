a5c61495f907a1f01b1555afed9e28af1cf22966
nayankhode23 <nayankhode23@gmail.com>
2025-12-30 06:12:44 +0000

    Refactor backend configuration in backend.tf

--- envs/dev/backend.tf
+++ envs/dev/backend.tf
@@ -4,6 +4,6 @@ terraform {
     key            = "envs/dev/terraform.tfstate"
     region         = "ap-south-1"
     encrypt        = true
-    #dynamodb_table = "three-tier-terraform-locks" # comment this if you don't use DynamoDB
   }
 }
+
